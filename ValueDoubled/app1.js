9f5106d06088f57469befdc3553b7c41640e994d
Maura Kummer <maurkummer@yahoo.com>
2022-04-21 08:57:55 +0200

    first commit

---
+++ ValueDoubled/app1.js
@@ -0,0 +1,12 @@
+//Given an array of integers, return a new array with each value doubled.
+
+//It is always going to be integers
+//For example:
+//[ 1,2,3] -->[2,4,6]
+//[8,2,3] -->[16,4,3]
+
+//Make a function that takes in an array
+//Map throught the array and multiply each element by 2
+
+const maps = (arr) => arr.map((element) => element * 2);
+console.log(maps[(1, 2, 4)], [2, 4, 8]); //the expected outcome is the array on the right
