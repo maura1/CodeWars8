bbcbca3d6855761a4c48c6088d0cb8e15794410e
Maura Kummer <maurkummer@yahoo.com>
2022-04-26 19:01:54 +0200

    correction, use spreat operator to create an array,plus the map()

---
+++ ReversedSequenced/app7.js
@@ -0,0 +1,18 @@
+/*Build a function that returns an array of integers from n to 1 where n>0.
+
+Example : n=5 --> [5,4,3,2,1]
+
+Test console.log(reverseSeq(5),[5,4,3,2,1]*/
+
+/*const reverseSeq= n =>{
+    let arr = []
+    for (i =n; i>=1; i--){
+      arr.push(i)
+    }
+    return arr;
+  
+  }*/
+
+//Using the arrow function
+
+const reverseSeq = (n) => [...Array(n)].map((x, i) => n - i);
