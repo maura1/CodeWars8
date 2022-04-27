3dd80beb351583a205565c0bc65a454d065bf50d
Maura Kummer <maurkummer@yahoo.com>
2022-04-27 18:02:10 +0200

    searching for an element begining from a certain index

--- ArrayContainsValue/app4.js
+++ ArrayContainsValue/app4.js
@@ -19,3 +19,11 @@ console.log(check([2, 3], 4));
 
 //alternative
 const check = (a, x) => a.includes(x);
+
+//When comparing strings and characters, includes is case sensitive
+
+//We can also check if some element is included starting from a certain element
+let arr = [1, 2, 3];
+console.log(arr.includes(3, 1)); //True
+console.log(arr.includes(3, 4)); //False (index 4 is not in the array)
+console.log(arr.includes(3, -100)); //True (if the computed index is less than 0 the entire index will be checked)
